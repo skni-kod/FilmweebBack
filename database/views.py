6530a5612684b9361fea025b6f657642c1fcd854
saimon833 <74502684+saimon833@users.noreply.github.com>
2022-06-15 17:43:53 +0200

    Fixed HTTP status

--- database/views.py
+++ database/views.py
@@ -3,6 +3,7 @@ from django import views
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_list_or_404, get_object_or_404
 from rest_framework import viewsets
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.views import APIView
