e9512905762c3c2abda45c49f74aa76ab55f9f89
Mariusz Dąbrowski <57808812+marioooo0o@users.noreply.github.com>
2022-12-07 01:35:55 +0100

    feat: seed database
    Seed genders, departments and languages from file
    https://github.com/orgs/skni-kod/projects/4/views/1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call([
+            DepartmentSeeder::class,
+            LanguageSeeder::class,
+            GenderSeeder::class
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
